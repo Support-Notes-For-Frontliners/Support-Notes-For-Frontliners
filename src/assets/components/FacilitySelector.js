80741d2b1d216c569d705ea3d1dd9e6ab0c5e31c
trevorkw7 <54345106+trevorkw7@users.noreply.github.com>
2020-05-22 22:30:20 -0700

    Added glow emphasis to facilities who need more attention

--- src/assets/components/FacilitySelector.js
+++ src/assets/components/FacilitySelector.js
@@ -16,10 +16,17 @@ import Grid from '@material-ui/core/Grid';
 const useStyles = makeStyles({
     root: {
         maxWidth: 345,
+       
     },
     rootSelected: {
         maxWidth: 345,
         backgroundColor: "#C8E8EA"
+    },
+    rootEmphasized: {
+        maxWidth: 345,
+        boxShadow: "2px 2px 33px 9px rgba(62,197,207,1)",
+        WebkitAnimation: "breathing 7s ease-out"
+
     },
     media: {
         height: 140,
@@ -32,11 +39,20 @@ const useStyles = makeStyles({
 
 export default function FacilitySelector({stepperCallback, jsonKey, elementSelected}) {
 
+    const[itemSelected, setItemSelected] = React.useState(false);
+
     const classes = useStyles();
+
+    React.useEffect(()=>{
+        if(elementSelected){
+            setItemSelected(true)
+        }
+    },[elementSelected])
 
     function handleInput(e){
         e.preventDefault();
         stepperCallback(e.currentTarget.id)
+        setItemSelected(true);
         // console.log(e.currentTarget.id);
     }
 
@@ -46,8 +62,9 @@ export default function FacilitySelector({stepperCallback, jsonKey, elementSelec
                 <Grid container justify="center" spacing="3">
                     <React.Fragment>
                         {location_list[jsonKey].map((selectedHospital, index) => (
-                            <Grid key={selectedHospital.name} item>
-                                <Card id={selectedHospital.name} className={elementSelected === selectedHospital.name ? classes.rootSelected : classes.root}>
+                            <Grid  key={selectedHospital.name} item>
+                                <Card id={selectedHospital.name} className={elementSelected === selectedHospital.name ? classes.rootSelected
+                                : selectedHospital["emphasis"] && !itemSelected ? classes.rootEmphasized : classes.root}>
                                     <CardActionArea>
                                         <CardMedia
                                             alt="increase priority"
